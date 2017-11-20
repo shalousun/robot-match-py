9c33730505af4c2f988da6f16385ec80390db14e
oppofind <836575280@qq.com>
2017-11-20 09:30:09 +0800

    修改oracle

--- OracleProvider.py
+++ OracleProvider.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+os.environ['NLS_LANG'] = '.AL32UTF8'
 import cx_Oracle
 
 class OracleProvider:
@@ -9,27 +11,27 @@ class OracleProvider:
         sql = ""
         try:
             cursor = self.connect.cursor()
-            row = cursor.excute(sql)
-            return row
+            cursor.execute(sql)
+            rs = cursor.fetchall()
+            return rs
         except (cx_Oracle.DatabaseError) as ex:
             print(ex)
-        finally:
-            self.connect.close()
 
     def get_fields_info(self, table_name):
-        if table_name.trip() == "":
+        if table_name.strip() == "":
             sql = "select distinct column_name，data_type,data_length,nullable,high_value,low_value,data_precision from user_tab_cols"
         else:
             sql = "select distinct column_name，data_type,data_length,nullable,high_value,low_value,data_precision from user_tab_cols where table_name ="+table_name
         try:
             cursor = self.connect.cursor()
-            row = cursor.excute(sql)
-            return row
+            cursor.execute(sql)
+            rs = cursor.fetchall()
+            return rs
         except (cx_Oracle.DatabaseError) as ex:
             print(ex)
         finally:
+            cursor.close()
             self.connect.close()
-
 
 def get_connect():
     try:
@@ -43,4 +45,5 @@ if __name__ == '__main__':
     print("main")
     connect = get_connect()
     oracle_provider = OracleProvider(connect)
-    oracle_provider.get_fields_info("hello")
+    rs = oracle_provider.get_fields_info("")
+    print(rs)
