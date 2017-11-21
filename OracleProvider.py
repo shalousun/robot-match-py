5bb64321725615f66d954ad6bf137a100cc94ace
oppofind <836575280@qq.com>
2017-11-21 17:44:04 +0800

    对oracle的字段信息做处理

--- OracleProvider.py
+++ OracleProvider.py
@@ -24,33 +24,37 @@ class OracleProvider:
     def get_fields_info(self, table_name):
         if table_name and table_name.strip():
             # table_name is not None AND table_nameg is not empty or blank
-            sql = "SELECT COL.table_name, col.column_name, col.data_type, COL.data_length, COL.nullable , COL.high_value," \
+            sql = "SELECT COL.table_name, col.column_name, col.data_type, COL.data_length," \
+                  "CASE COL.nullable WHEN 'Y' THEN '1' ELSE '0' end as \"nullable\", COL.high_value," \
                   " COL.low_value, COL.data_precision, CASE uc.constraint_type " \
-                  "WHEN 'P' THEN 'yes' ELSE '' END AS \"PRIMARY_KEY\" FROM user_tab_columns col LEFT JOIN " \
+                  "WHEN 'P' THEN '1' ELSE '0' END AS \"PRIMARY_KEY\" FROM user_tab_columns col LEFT JOIN " \
                   "user_cons_columns ucc ON ucc.table_name = col.table_name AND ucc.column_name = col.column_name " \
                   "LEFT JOIN user_constraints uc ON uc.constraint_name = ucc.constraint_name AND uc.constraint_type = 'P' " \
                   "where  COL.TABLE_NAME = '" + table_name + "'"
         else:
             # table_name is None OR table_name is empty or blank
-            sql = "SELECT COL.table_name, col.column_name, col.data_type, COL.data_length, COL.nullable , COL.high_value," \
+            sql = "SELECT COL.table_name, col.column_name, col.data_type, COL.data_length," \
+                  "CASE COL.nullable WHEN 'Y' THEN '1' ELSE '0' end as \"nullable\", COL.high_value," \
                   " COL.low_value, COL.data_precision , CASE uc.constraint_type " \
                   "WHEN 'P' THEN 'yes' ELSE '' END AS \"PRIMARY_KEY\" FROM user_tab_columns col LEFT JOIN " \
                   "user_cons_columns ucc ON ucc.table_name = col.table_name AND ucc.column_name = col.column_name " \
                   "LEFT JOIN user_constraints uc ON uc.constraint_name = ucc.constraint_name AND uc.constraint_type = 'P'"
+        result_list = []
         try:
             cursor = self.connect.cursor()
             cursor.execute(sql)
             rs = cursor.fetchall()
-            return rs
+            for row in rs:
+                tem = list(row)
+                # convert data type value
+                tem[2] = df.proccess_data_type(row[2])
+                result_list.append(tem)
+            return result_list
         except cx_Oracle.DatabaseError as ex:
             print(ex)
         finally:
             cursor.close()
             self.connect.close()
-
-    # 对数据类型做转换
-
-
 
 def get_connect():
     try:
@@ -64,10 +68,9 @@ if __name__ == '__main__':
     print("main")
     connect = get_connect()
     oracle_provider = OracleProvider(connect)
-    rs = oracle_provider.get_fields_info("")
+    rs = oracle_provider.get_fields_info("TB_24XSRCYJL")
     rs_dataframe = pd.DataFrame(list(rs), columns=['table_name', 'column_name', 'data_type', 'data_length', 'nullable',
                                                    'high_value', 'low_value', 'data_precision', 'primary_key'])
 
-    # print(rs_dataframe)
-    data = df.proccess_data_type("char")
-    print(data)
+    print(rs_dataframe)
+
